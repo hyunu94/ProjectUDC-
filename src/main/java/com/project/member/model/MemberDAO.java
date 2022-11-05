c264ea46517b846163824417b7795a6ae37b7601
hyunu94 <zkvmfltjs7@naver.com>
2022-11-05 22:00:33 +0800

    회원가입 DAO

--- src/main/java/com/project/member/model/MemberDAO.java
+++ src/main/java/com/project/member/model/MemberDAO.java
@@ -13,7 +13,7 @@ public class MemberDAO {
 		pool = new ConnectionPoolMgr2();
 	}
 	
-	public int insertMember(MemberVO vo) throws SQLException { //판매자 회원가입시
+	public int insertSeller(MemberVO vo) throws SQLException { //판매자 회원가입시
 		Connection con = null;
 		PreparedStatement ps = null;
 		int cnt = 0;
@@ -22,7 +22,7 @@ public class MemberDAO {
 			con = pool.getConnection();
 			
 			String sql = "insert into member(memberNo,userid,pwd,name,nick,jumin,mobile,kindNo,locationNo)\r\n"
-					+ "values(member_seq.nextval,?,?,?,?,?,?,?,?)";
+					+ "values(member_seq.nextval,?,?,?,?,?,?,1,?)";
 			ps = con.prepareStatement(sql);
 			ps.setString(1, vo.getUserid());
 			ps.setString(2, vo.getPwd());
@@ -30,8 +30,7 @@ public class MemberDAO {
 			ps.setString(4, vo.getNick());
 			ps.setString(5, vo.getJumin());
 			ps.setString(6, vo.getMobile());
-			ps.setInt(7, vo.getKindNo());
-			ps.setInt(8, vo.getLocationNo());
+			ps.setInt(7, vo.getLocationNo());
 			
 			cnt = ps.executeUpdate();
 			
@@ -43,4 +42,62 @@ public class MemberDAO {
 			pool.dbClose(ps, con);
 		}
 	}
+	
+	public int insertMember(MemberVO vo) throws SQLException { //일반회원 회원가입시
+		Connection con = null;
+		PreparedStatement ps = null;
+		int cnt = 0;
+		
+		try {
+			con = pool.getConnection();
+			
+			String sql = "insert into member(memberNo,userid,pwd,name,nick,jumin,mobile,kindNo,locationNo)\r\n"
+					+ "values(member_seq.nextval,?,?,?,?,?,\r\n"
+					+ "?,2,?);";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, vo.getUserid());
+			ps.setString(2, vo.getPwd());
+			ps.setString(3, vo.getName());
+			ps.setString(4, vo.getNick());
+			ps.setString(5, vo.getJumin());
+			ps.setString(6, vo.getMobile());
+			ps.setInt(7, vo.getLocationNo());
+			
+			cnt = ps.executeUpdate();
+			
+			if(cnt>0) {
+				System.out.println("생성된 판매자 정보 : " + vo);
+			}
+			return cnt;
+		}finally {
+			pool.dbClose(ps, con);
+		}
+	}
+	
+	public int insertBemember(MemberVO vo) throws SQLException { //비회원 기능이용시
+		Connection con = null;
+		PreparedStatement ps = null;
+		int cnt = 0;
+		
+		try {
+			con = pool.getConnection();
+			
+			String sql = "insert into member(memberNo,name,mobile,kindNo)\r\n"
+					+ "values(member_seq.nextval,?,?,3);";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, vo.getName());
+			ps.setString(2, vo.getMobile());
+		
+			cnt = ps.executeUpdate();
+			
+			if(cnt>0) {
+				System.out.println("생성된 비회원 정보 : " + vo);
+			}
+			return cnt;
+		}finally {
+			pool.dbClose(ps, con);
+		}
+	}
+	
+	
 }
